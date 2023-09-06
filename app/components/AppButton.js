1802e481fc3757ee71dbd52c505e9445433376c9
Daniel Somade <flexsidee@gmail.com>
2023-09-06 21:35:08 +0100

    modified the click event on the buttons

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -1,9 +1,9 @@
 import {
 	Dimensions,
+	Pressable,
 	StyleSheet,
 	Text,
 	TouchableOpacity,
-	View,
 } from "react-native";
 
 import colors from "../config/colors";
@@ -16,21 +16,26 @@ const AppButton = ({
 	onPress,
 }) => {
 	return (
-		<TouchableOpacity onPress={onPress}>
-			{bigButton ? (
-				<View style={[styles.bigButton, { backgroundColor: colors[bg] }]}>
-					<Text style={[styles.text, { color: colors[color] }]}>
-						{children}
-					</Text>
-				</View>
-			) : (
-				<View style={[styles.button, { backgroundColor: colors[bg] }]}>
-					<Text style={[styles.text, { color: colors[color] }]}>
-						{children}
-					</Text>
-				</View>
-			)}
-		</TouchableOpacity>
+		<Pressable
+			onPress={onPress}
+			style={({ pressed }) => [
+				styles.button,
+				bigButton ? styles.bigButton : styles.normalButton,
+				{
+					backgroundColor: pressed
+						? colors[
+								bg === "darkGray"
+									? "gray"
+									: bg === "lightGray"
+									? "white"
+									: "lightOrange"
+						  ]
+						: colors[bg],
+				},
+			]}
+		>
+			<Text style={[styles.text, { color: colors[color] }]}>{children}</Text>
+		</Pressable>
 	);
 };
 
@@ -40,22 +45,24 @@ const { width: screenWidth } = Dimensions.get("window");
 const buttonWidth = screenWidth / 4 - 17;
 
 const styles = StyleSheet.create({
-	bigButton: {
+	button: {
 		borderRadius: 50,
-		justifyContent: "center",
-		paddingLeft: 30,
 		height: buttonWidth,
+		justifyContent: "center",
+	},
+	bigButton: {
+		paddingLeft: 30,
 		width: buttonWidth * 2,
 	},
-	button: {
+	normalButton: {
 		alignItems: "center",
-		borderRadius: 50,
-		justifyContent: "center",
-		height: buttonWidth,
 		width: buttonWidth,
 	},
 	text: {
 		fontFamily: "Helvetica Neue",
 		fontSize: 40,
+	},
+	test: {
+		// backgroundColor: "red",
 	},
 });
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,8 @@
 export default {
 	black: "#000",
 	darkGray: "#505050",
+	gray: "#808080",
+	lightOrange: "#FFA500",
 	lightGray: "#D4D4D2",
 	orange: "#FF9500",
 	white: "#FFFFFF",
